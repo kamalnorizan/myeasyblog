e090a367b5dd6e368ed92a7690ec741504354beb
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2020-07-23 15:05:55 +0800

    laravel passport

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/post/allpost', 'ApiController@allpost');
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/post/allpost', 'ApiController@allpost');
+});
+
